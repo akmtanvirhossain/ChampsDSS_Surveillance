4657c1aa5e751ab09499691c172dcc6a756a0127
Tanvir Hossain <thossain@icddrb.org>
2017-04-09 09:35:34 +0600

    Tanvir

---
+++ app/src/main/java/org/icddrb/champsdsssur/LoginActivity.java
@@ -0,0 +1,339 @@
+package org.icddrb.champsdsssur;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import Common.Connection;
+import Common.Global;
+import Common.ProjectSetting;
+import Utility.MySharedPreferences;
+
+public class LoginActivity extends Activity {
+    public static final int DIALOG_DOWNLOAD_PROGRESS = 0;
+    Connection C;
+    Global g;
+    boolean networkAvailable = false;
+    int count = 0;
+    TextView lblStaffType;
+    String   SystemUpdateDT="";
+    private ProgressDialog dialog;
+    private  String Password="";
+    MySharedPreferences sp;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.login_activity);
+            C = new Connection(this);
+            g = Global.getInstance();
+            sp = new MySharedPreferences();
+            sp.save(this,"deviceid","");
+            sp.save(this,"userid","");
+
+            final TextView UniqueUserId      = (TextView)findViewById(R.id.UniqueUserId);
+            final Spinner uid      = (Spinner)findViewById(R.id.userId);
+            final EditText pass    = (EditText)findViewById(R.id.pass);
+            TextView lblSystemDate = (TextView)findViewById(R.id.lblSystemDate);
+
+            //Need to update date every time whenever shared updated system
+            //*********************************************************************
+            SystemUpdateDT = "04042017";  //Format: DDMMYYYY
+            lblSystemDate.setText("Version: 1.0, Built on:"+ SystemUpdateDT);
+
+            //Check for Internet connectivity
+            networkAvailable = Connection.haveNetworkConnection(LoginActivity.this);
+
+
+            //Rebuild Database
+            String TotalTab = C.ReturnSingleValue("SELECT count(*) FROM sqlite_master WHERE type = 'table' AND name != 'android_metadata' AND name != 'sqlite_sequence'");
+
+            if(Integer.valueOf(TotalTab) == 0)
+            {
+                if (networkAvailable)
+                {
+                    //Call Setting Form
+                    finish();
+                    Intent f1 = new Intent(getApplicationContext(), SettingForm.class);
+                    startActivity(f1);
+                    return;
+                }
+                else
+                {
+                    Connection.MessageBox(LoginActivity.this,"Internet connection is not available for building initial database.");
+                    return;
+                }
+            }else
+            {
+                C.CreateTable("LastVillBari","Create table LastVillBari(Vill varchar(3), Bari varchar(4))");
+            }
+
+
+            //Device Unique ID
+            final String UniqueID = C.ReturnSingleValue("Select DeviceId from DeviceList");
+            UniqueUserId.setText("Device ID :"+ UniqueID);
+            g.setDeviceNo(UniqueID);
+            sp.save(this,"deviceid",UniqueID);
+
+            //Only for removing the data of training: 17 Nov 2015
+            /*if(Global.DateNowDMY().equals("11/03/2017") | Global.DateNowDMY().equals("12/03/2017")  | Global.DateNowDMY().equals("13/03/2017") | Global.DateNowDMY().equals("14/03/2017") | Global.DateNowDMY().equals("15/03/2017"))
+            {
+                String Count = C.ReturnSingleValue("select count(*) total from DataCollector");
+                if(Integer.valueOf(Count)>87) {
+                    C.Save("Delete from DataCollector");
+                    C.Save("Delete from Baris");
+                }
+                C.Save("Delete from Household where date(endt)     <= '2017-03-15'");
+                C.Save("Delete from Visits where date(endt)        <= '2017-03-15'");
+                C.Save("Delete from SES where date(endt)           <= '2017-03-15'");
+                C.Save("Delete from PregHis where date(endt)       <= '2017-03-15'");
+                C.Save("Delete from Member where date(endt)        <= '2017-03-15'");
+            }*/
+
+
+            //**************************************************************************************
+            if (networkAvailable)
+            {
+                //Reqular data sync
+                C.Sync_DatabaseStructure(UniqueID);
+                C.Sync_Download("DataCollector",UniqueID,"Status='d'");
+                C.Sync_Download("RoundVisit",UniqueID,"CurrRound='1'");
+
+                //Intent syncService = new Intent(this, Sync_Service.class);
+                //startService(syncService);
+            }
+            //**************************************************************************************
+            uid.setAdapter(C.getArrayAdapter("select UserId||'-'||UserName User from DataCollector order by UserName"));
+            String[] CL = uid.getSelectedItem().toString().split("-");
+            uid.setSelection(Global.SpinnerItemPosition(uid,CL[0].length(),C.ReturnSingleValue("Select UserId from LastLogin")));
+
+            Button btnClose=(Button)findViewById(R.id.btnClose);
+            btnClose.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    //Connection.MessageBox(LoginActivity.this,MySharedPreferences.getValue(LoginActivity.this,"lat"));
+                    finish();
+                    System.exit(0);
+                }
+            });
+
+            //Login -----------------------------------------------------------------------
+            Button loginButton = (Button) findViewById(R.id.btnLogin);
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    try
+                    {
+                        String[] U = Connection.split(uid.getSelectedItem().toString(),'-');
+                        g.setUserId(U[0]);
+
+                        if (!C.Existence("Select * from DataCollector where UserId='" + U[0] + "' and Pass='" + pass.getText().toString() + "'"))
+                        {
+                            Connection.MessageBox(LoginActivity.this,"This is not a valid user id or password");
+                            return;
+                        }
+
+                        //Store Last Login information
+                        C.Save("Delete from LastLogin");
+                        C.Save("Insert into LastLogin(UserId)Values('"+ U[0] +"')");
+
+
+                        //Download Updated System
+                        //...................................................................................
+                        if (networkAvailable == true)
+                        {
+                            //Retrieve data from server for checking local device
+                            String[] ServerVal  = Connection.split(C.ReturnResult("ReturnSingleValue","sp_ServerCheckDSS '"+ UniqueID +"'"),',');
+                            String ServerDate   = ServerVal[0].toString();
+                            String UpdateDT     = ServerVal[1].toString();
+
+                            //Check for New Version
+                            if (!UpdateDT.equals(SystemUpdateDT)) {
+                                SystemDownload d = new SystemDownload();
+                                d.setContext(getApplicationContext());
+                                d.execute(Global.UpdatedSystem);
+                            }
+                            else
+                            {
+                                //check for system date
+                                if(ServerDate.equals(Global.TodaysDateforCheck())==false)
+                                {
+                                    Connection.MessageBox(LoginActivity.this, "আপনার ট্যাব এর তারিখ সঠিক নয় ["+ Global.DateNowDMY() +"]।");
+                                    startActivity(new Intent(android.provider.Settings.ACTION_DATE_SETTINGS));
+                                    return;
+                                }
+
+                                final ProgressDialog progDailog = ProgressDialog.show(LoginActivity.this, "", "Please Wait . . .", true);
+
+                                new Thread() {
+                                    public void run() {
+                                        try {
+                                            finish();
+                                            Intent f1 = new Intent(getApplicationContext(),MainMenu.class);
+                                            startActivity(f1);
+                                        } catch (Exception e) {
+
+                                        }
+                                        progDailog.dismiss();
+                                    }
+                                }.start();
+                            }
+                        }
+                        else
+                        {
+                            final ProgressDialog progDailog = ProgressDialog.show(LoginActivity.this, "", "Please Wait . . .", true);
+
+                            new Thread() {
+                                public void run() {
+                                    try {
+                                        finish();
+                                        Intent f1 = new Intent(getApplicationContext(),MainMenu.class);
+                                        startActivity(f1);
+                                    } catch (Exception e) {
+
+                                    }
+                                    progDailog.dismiss();
+                                }
+                            }.start();
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        //Connection.MessageBox(LoginActivity.this, ex.getMessage());
+                        //return;
+                        final ProgressDialog progDailog = ProgressDialog.show(LoginActivity.this, "", "Please Wait . . .", true);
+
+                        new Thread() {
+                            public void run() {
+                                try {
+                                    finish();
+                                    Intent f1 = new Intent(getApplicationContext(),MainMenu.class);
+                                    startActivity(f1);
+                                } catch (Exception e) {
+
+                                }
+                                progDailog.dismiss();
+                            }
+                        }.start();
+                    }
+                }
+            });
+        }
+        catch(Exception ex)
+        {
+            Connection.MessageBox(LoginActivity.this, ex.getMessage());
+        }
+    }
+
+    //Install application
+    private void InstallApplication()
+    {
+        File apkfile = new File(Environment.getExternalStorageDirectory() + File.separator + ProjectSetting.NewVersionName +".apk");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // without this flag android returned a intent error!
+        intent.setDataAndType(Uri.parse("file://" + apkfile.toString()), "application/vnd.android.package-archive");
+
+        startActivity(intent);
+    }
+
+    //Downloading updated system from the central server
+    class SystemDownload extends AsyncTask<String, String, Void> {
+        private Context context;
+
+        public void setContext(Context contextf){
+            context = contextf;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(LoginActivity.this);
+            dialog.setMessage("Downloading Updated System...");
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            dialog.setCancelable(false);
+            dialog.show();
+        }
+
+
+        protected void onProgressUpdate(String... progress) {
+            dialog.setProgress(Integer.parseInt(progress[0]));
+            //publishProgress(progress);
+
+        }
+
+        //@Override
+        protected void onPostExecute(String unused) {
+            dialog.dismiss();
+        }
+
+
+        @Override
+        protected Void doInBackground(String... arg0) {
+            try {
+                URL url = new URL(arg0[0]);
+                HttpURLConnection c = (HttpURLConnection) url.openConnection();
+                c.setRequestMethod("GET");
+                c.connect();
+                int lenghtOfFile = c.getContentLength();
+
+                File file=Environment.getExternalStorageDirectory();
+
+                file.mkdirs();
+                File outputFile = new File(file.getAbsolutePath()+ File.separator + ProjectSetting.NewVersionName +".apk");
+
+                if(outputFile.exists()){
+                    outputFile.delete();
+                }
+                else
+                {
+                    outputFile.createNewFile();
+                }
+
+                FileOutputStream fos = new FileOutputStream(outputFile);
+
+                InputStream is = c.getInputStream();
+
+
+                byte[] buffer = new byte[1024];
+                int len1 = 0;
+                long total = 0;
+                while ((len1 = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, len1);
+                    count++;
+                }
+                fos.close();
+                is.close();
+
+
+                InstallApplication();
+
+                dialog.dismiss();
+
+            } catch (IOException e) {
+                //Log.e("UpdateAPP", "Update error! " + e.getMessage());
+            }
+            return null;
+        }
+    }
+
+}
+
---
+++ app/src/main/java/org/icddrb/champsdsssur/MainMenu.java
@@ -0,0 +1,282 @@
+package org.icddrb.champsdsssur;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Common.Connection;
+import Common.Global;
+import Common.Tran_Download;
+
+public class MainMenu extends Activity {
+
+    static String DEVICEID = "";
+    Button cmdDataSync;
+    Button cmdHHInterview;
+    Button cmdMember;
+
+    Connection C;
+    Global g;
+    Spinner spnCluster;
+    Spinner spnBlock;
+    Spinner spnRound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        try {
+            super.onCreate(savedInstanceState);
+            //getLayoutInflater().inflate(R.layout.main_menu, frameLayout);
+            setContentView(R.layout.main_menu);
+            C = new Connection(this);
+            g = Global.getInstance();
+
+            turnGPSOn();
+            Intent gpsService = new Intent(this, GPSService.class);
+            startService(gpsService);
+
+            DEVICEID = g.getDeviceNo();
+            /*
+            cmdHHInterview = (Button) findViewById(R.id.cmdHHInterview);
+            cmdHHInterview.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    finish();
+                    Intent f1 = new Intent(getApplicationContext(), Household_list.class);
+                    Bundle bundle=new Bundle();
+                    bundle.putString("village", "");
+                    bundle.putString("VCode", "");
+                    //f1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    f1.putExtras(bundle);
+                    startActivity(f1);
+                    //startActivity(new Intent(MainMenu.this, Household_list.class));
+                }
+            });
+            */
+
+            spnRound = (Spinner)findViewById(R.id.spnRound);
+            spnRound.setAdapter(C.getArrayAdapter("Select RoundNo from RoundVisit order by RoundNo desc limit 4"));
+            spnCluster = (Spinner)findViewById(R.id.spnCluster);
+            spnCluster.setAdapter(C.getArrayAdapter("Select distinct Cluster from Baris"));
+            spnBlock = (Spinner)findViewById(R.id.spnBlock);
+            spnBlock.setAdapter(C.getArrayAdapter("Select distinct Block from Baris"));
+
+
+            cmdHHInterview = (Button) findViewById(R.id.cmdHHInterview);
+            cmdHHInterview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Bundle IDbundle = new Bundle();
+                    IDbundle.putString("Village", "");
+                    IDbundle.putString("VCode", "");
+                    IDbundle.putString("roundno", spnRound.getSelectedItem().toString());
+                    IDbundle.putString("cluster", spnCluster.getSelectedItem().toString());
+                    IDbundle.putString("block", spnBlock.getSelectedItem().toString());
+                    finish();
+                    Intent f1;
+                    f1 = new Intent(getApplicationContext(), Household_list.class);
+                    f1.putExtras(IDbundle);
+                    startActivity(f1);
+                    //startActivity(new Intent(MainMenu.this, HouseholdIndex1.class));
+                }
+            });
+
+//            cmdMember = (Button) findViewById(R.id.cmdMember);
+//            cmdMember.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Bundle IDbundle = new Bundle();
+//                    IDbundle.putString("Village", "");
+//                    IDbundle.putString("VCode", "");
+//                    finish();
+//                    Intent f1;
+//                    f1 = new Intent(getApplicationContext(), Member_list.class);
+//                    f1.putExtras(IDbundle);
+//                    startActivity(f1);
+//                    //startActivity(new Intent(MainMenu.this, HouseholdIndex1.class));
+//                }
+//            });
+
+            cmdDataSync = (Button) findViewById(R.id.cmdDataSync);
+            cmdDataSync.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    //Check for Internet connectivity
+                    if (Connection.haveNetworkConnection(MainMenu.this)) {
+                    } else {
+                        Connection.MessageBox(MainMenu.this, "ডেটা সিঙ্ক করার জন্য  ইন্টারনেট সংযোগ নাই.");
+                        return;
+                    }
+
+                    AlertDialog.Builder adb = new AlertDialog.Builder(MainMenu.this);
+                    adb.setTitle("Data Sync");
+                    adb.setMessage("আপনি ডেটা সিঙ্ক করতে চান[হ্যাঁ/না]?");
+                    adb.setNegativeButton("না", null);
+                    adb.setPositiveButton("হ্যাঁ", new AlertDialog.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            C = new Connection(MainMenu.this);
+                            final ProgressDialog progDailog = ProgressDialog.show(MainMenu.this, "", "Please Wait . . .", true);
+
+                            new Thread() {
+                                public void run() {
+                                    try {
+                                        String r = C.ExecuteCommandOnServer("Insert into UploadMonitor(DeviceID)Values('"+ DEVICEID +"')");
+
+                                        Tran_Download td = new Tran_Download(MainMenu.this);
+
+                                        //GPS Bari List
+                                        td.Sync_Download("Baris",DEVICEID,"");
+
+                                        List<String> tableList = new ArrayList<String>();
+                                        tableList.add("Baris");
+                                        tableList.add("Household");
+                                        tableList.add("Visits");
+                                        tableList.add("Member");
+                                        tableList.add("SES");
+                                        tableList.add("PregHis");
+
+                                        C.Sync_Upload(tableList);
+
+                                    } catch (Exception e) {
+
+                                    }
+                                    progDailog.dismiss();
+                                }
+                            }.start();
+
+                        }
+                    });
+                    adb.show();
+                }
+            });
+
+            Button cmdExit = (Button) findViewById(R.id.cmdExit);
+            cmdExit.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    AlertDialog.Builder adb = new AlertDialog.Builder(MainMenu.this);
+                    adb.setTitle("Exit");
+                    adb.setMessage("আপনি কি এই সিস্টেম থেকে বের হতে চান [হ্যাঁ/না]?");
+                    adb.setNegativeButton("না", null);
+                    adb.setPositiveButton("হ্যাঁ", new AlertDialog.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            /*BroadcastReceiver rec = new BroadcastReceiver()
+                            {
+                                String lat = "";
+                                String lon = "";
+                                @Override
+                                public void onReceive(Context context, Intent intent)
+                                {
+                                    String action = intent.getAction();
+                                    if(action.equals("gps_data")){
+                                        lat = intent.getExtras().getString("Latitude");
+                                        lon = intent.getExtras().getString("Longitude");
+                                    }
+                                }
+                            };
+                            IntentFilter filter = new IntentFilter("gps_data");
+                            registerReceiver(rec, filter);*/
+
+                            Intent gpsService = new Intent(MainMenu.this, GPSService.class);
+                            stopService(gpsService);
+                            finish();
+                        }
+                    });
+                    adb.show();
+                }
+            });
+
+
+
+            /* Start Usage of SharedPreferences class */
+            /*
+            final MySharedPreferences mySharedPreferences = new MySharedPreferences();
+            final EditText txtSPrefInput = (EditText) findViewById(R.id.txtSPrefInput);
+            final EditText txtSPrefOutput = (EditText) findViewById(R.id.txtSPrefOutput);
+            Button cmdSPrefSave = (Button) findViewById(R.id.cmdSPrefSave);
+            cmdSPrefSave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mySharedPreferences.save(MainMenu.this, "KeyName", txtSPrefInput.getText().toString());
+                }
+            });
+            Button cmdSPrefGet = (Button) findViewById(R.id.cmdSPrefGet);
+            cmdSPrefGet.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String value = mySharedPreferences.getValue(MainMenu.this, "KeyName");
+                    txtSPrefOutput.setText(value);
+                }
+            });
+            Button cmdSPrefRemove = (Button) findViewById(R.id.cmdSPrefRemove);
+            cmdSPrefRemove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mySharedPreferences.removeValue(MainMenu.this, "KeyName");
+                    txtSPrefOutput.setText("Removed");
+                }
+            });
+            Button cmdSPrefClear = (Button) findViewById(R.id.cmdSPrefClear);
+            cmdSPrefClear.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mySharedPreferences.clearSharedPreference(MainMenu.this);
+                }
+            });
+            */
+            /* End Usage of SharedPreferences class */
+
+
+        } catch (Exception ex)
+        {
+            Connection.MessageBox(MainMenu.this,ex.getMessage());
+        }
+    }
+
+
+    // Method to turn on GPS
+    public void turnGPSOn(){
+        try
+        {
+            String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            if(!provider.contains("gps")){ //if gps is disabled
+                final Intent poke = new Intent();
+                poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+                poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+                poke.setData(Uri.parse("3"));
+                sendBroadcast(poke);
+            }
+        }
+        catch (Exception e) {
+        }
+    }
+
+    // Method to turn off the GPS
+    public void turnGPSOff(){
+        String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+
+        if(provider.contains("gps")){ //if gps is enabled
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            sendBroadcast(poke);
+        }
+    }
+
+    // turning off the GPS if its in on state. to avoid the battery drain.
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        turnGPSOff();
+    }
+}
